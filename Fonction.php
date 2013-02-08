35f87e59402d9b9a777cf3fdc746b53abe31bde6
thom1709 <thom1709@hotmail.fr>
2013-02-08 21:56:03 +0100

    Correction rapide des fonctions de Rocco. Tout est a refaire

--- Fonction.php
+++ Fonction.php
@@ -1,13 +1,18 @@
 <?php
+//Il est où, le fichier de connexion ??? MERDE ROCCO!!!!!!!!!
 include("connexion.php");
 
-function genere() {
-    $cle = md5(uniqid(rand(0, 9999)));
-    insereCle($pseudo, $cle);
+$key = generateKey();
+insertKey($key);
+
+function generateKey() {
+	//TODO creer une vraie clé !!!!! MERDE ROCCO!!!!!!!!!
+    $key = md5(uniqid(rand(0, 9999)));
+    return $key;
 }
 
-function insereCle($pseudo, $cle) {
-    $sql = mysql_query("INSERT INTO table VALUES(".$pseudo.", ".$cle."");
-    return $cle;   
+function insertKey($pseudo, $key) {
+	//TODO faire une vraie fonction qui vérifie qu'il n'y a pas d'erreur !!!!! ROCCO, on t'a répété à plusieurs reprises d'utiliser PDO !!!! MERDE !!!!!!!!
+    $key = mysql_query("INSERT INTO table VALUES(".$pseudo.", ".$cle.""); 
 }
 ?>
